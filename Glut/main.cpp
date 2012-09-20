5dac726d5e88b479c3898e6b673428fd5ec2cc94
Petronio Candido de Lima e Silva <petronio.candido@gmail.com>
2012-09-20 17:09:37 -0300

    - Correções na classe Glut/Image

--- Glut/main.cpp
+++ Glut/main.cpp
@@ -1,4 +1,21 @@
-
+/*
+ * CG - Códigos gerados para a disciplina de Computação Gráfica
+ * Copyright (C) 2012 Petrônio Cândido de Lima e Silva.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
 #include <GL/glut.h>
 #include <stdlib.h>
 #include "Image.h"
@@ -24,9 +41,12 @@ static void display(void)
 */
 
     Image img((char *)"teste.bmp");
-    Image i2 = img.blur();
+    Image i2 = img.blur(); //.resize(100,400);
+    
+    i2 = i2.resize(100,200);
+    
     i2.draw();
-    //img.draw();   
+//    img.draw();   
    
    
     glutSwapBuffers();
